0c5b6ca8b4fc86c97180aca70d3ea035775595c8
a944409629 <yangzhiquan@fanydeimac13.local>
2016-06-23 16:45:52 +0800

    Initial Commit

---
+++ JSQ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSQ
+//
+//  Created by 杨志权 on 16/6/23.
+//  Copyright © 2016年 杨泽奇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
